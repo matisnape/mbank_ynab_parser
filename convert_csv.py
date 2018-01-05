cea8cfa24c980b9496ea3cc07cf608dd2766f96a
Anna Anks Nowak <anna.nowak@netguru.pl>
2018-01-05 16:35:25 +0100

    adding some additional rules

--- convert_csv.py
+++ convert_csv.py
@@ -1,6 +1,7 @@
 import csv
 import sys
 import getopt
+import re
 
 def main(argv):
     input_file = ''
@@ -21,25 +22,55 @@ def main(argv):
     convert_csv(input_file, output_file)
 
 def convert_csv(input_csv, new_csv):
-    headers = [
-        '#Data operacji', '#Opis operacji', '#Tytuł', '#Nadawca/Odbiorca', '#Numer konta', '#Kwota'
+    HEADERS = [
+        'DATE', '#Opis operacji', 'MEMO', 'PAYEE', '#Numer konta', 'AMOUNT'
     ]
+    REGULARNE = 'PRZELEW REGULARNE OSZCZ'
+    SPLATA_KART = 'ATA KARTY KREDYT.'
+    KARTA = 'KARTA'
+    KAPITALIZACJA = 'KAPITALIZACJA ODSETEK'
+    PODATEK_ODSETKI = 'PODATEK OD ODSETEK KAPIT'
+    MBANK_DELIMITER = ';'
 
     with open(input_csv, 'r', encoding='cp1250') as csv_file:
         csvRows = csv_file.readlines()[38:-5]
-        transactions_list = []
+        transactions_list = [HEADERS]
         for row in csvRows:
-            new_row = row.split(';')
+            new_row = row.split(MBANK_DELIMITER)
             new_row = new_row[:-2]
             new_row[6] = (new_row[6][:-3] + '.' + new_row[6][-2:]).replace(' ', '')
+            # remove Data księgowania
             new_row.pop(1)
+            # merge Numer konta with Memo
+            if new_row[4] != '':
+                new_row[2] = new_row[2] + new_row[4]
+            # REGULARNE OSZCZĘDZANIE
+            if REGULARNE in new_row[1]:
+                new_row[3] = 'Transfer: Regularne Oszczedzanie'
+            # Credit Card pay up
+            if SPLATA_KART in new_row[1] and KARTA in new_row[2]:
+                new_row[3] = 'Transfer: Kredytowka'
+            # Kapitalizacja & Odsetki
+            if KAPITALIZACJA in new_row[1]:
+                new_row[3] = 'Kapitalizacja'
+            if PODATEK_ODSETKI in new_row[1]:
+                new_row[3] = 'Odsetki'
+            # if Payee empty, move Memo there
+            if new_row[3] == '"  "':
+                new_row[3] = new_row[2]
+                new_row[2] = ''
+            # make strings prettier
+            for item in new_row:
+                item = re.sub('["\']', '', item)
+            print(new_row)
             transactions_list.append(new_row)
-        transactions_list.insert(0, headers)
         write_file(transactions_list, new_csv)
 
 def write_file(data, new_csv):
-    with open(new_csv, 'w', encoding='cp1250') as converted_file:
-        csvWriter = csv.writer(converted_file, delimiter=';')
+    YNAB_DELIMITER = ';'
+
+    with open(new_csv, 'w', encoding='utf-8') as converted_file:
+        csvWriter = csv.writer(converted_file, delimiter=YNAB_DELIMITER)
         for row in data:
             csvWriter.writerow(row)
 
