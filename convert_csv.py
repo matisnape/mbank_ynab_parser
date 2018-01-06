5d2ffd229cd6798b75a95bee34e04a68b94aa607
Anna Anks Nowak <anna.nowak@netguru.pl>
2018-01-06 17:37:43 +0100

    Extract converting row to a method

--- convert_csv.py
+++ convert_csv.py
@@ -20,6 +20,7 @@ INTERNAL_INCOMING = constants.INTERNAL_INCOMING
 KONTO_1 = constants.KONTO_1
 KONTO_2 = constants.KONTO_2
 KONTO_3 = constants.KONTO_3
+KONTO_REGULARNE = constants.KONTO_REGULARNE
 
 DATE_COL = 0
 OPIS_OPERACJI_COL = 1
@@ -54,50 +55,70 @@ def convert_csv(input_csv, new_csv):
     with open(input_csv, 'r', encoding='cp1250') as csv_file:
         csvRows = csv_file.readlines()[38:-5]
         transactions_list = [YNAB_HEADERS]
+
         for row in csvRows:
-            new_row = row.split(';')
-            new_row = new_row[:-2]
-            new_row[OLD_AMOUNT_COL] = (new_row[OLD_AMOUNT_COL][:-3] + '.' + new_row[OLD_AMOUNT_COL][-2:]).replace(' ', '')
-            # remove Data księgowania
-            new_row.pop(1)
-            # merge Numer konta with Memo
-            if new_row[NUMER_KONTA_COL] != '':
-                new_row[MEMO_COL] = new_row[MEMO_COL] + new_row[NUMER_KONTA_COL]
-            # REGULARNE OSZCZĘDZANIE
-            if REGULARNE in new_row[OPIS_OPERACJI_COL]:
-                new_row[PAYEE_COL] = 'Transfer: Regularne Oszczedzanie'
-            # Credit Card pay up
-            if SPLATA_KART in new_row[OPIS_OPERACJI_COL] and KARTA["id"] in new_row[MEMO_COL]:
-                new_row[PAYEE_COL] = 'Transfer: Kredytowka'
-            # Kapitalizacja & Odsetki
-            replace_other(new_row, KAPITALIZACJA)
-            replace_other(new_row, PODATEK_ODSETKI)
-            # if Payee empty, move Memo there
-            if new_row[PAYEE_COL] == '"  "':
-                new_row[PAYEE_COL] = new_row[MEMO_COL]
-                new_row[MEMO_COL] = ''
-            # transfer to accounts
-            replace_account(new_row, KONTO_1)
-            replace_account(new_row, KONTO_2)
-            replace_account(new_row, KONTO_3)
-            # make strings prettier
-            for item in new_row:
-                item = re.sub('["\']', '', item)
+            new_row = convert_row(row)
+
             # ignore transfer from own accounts
             # TODO: add option for this in the cmd
             if new_row[OPIS_OPERACJI_COL] != INTERNAL_INCOMING:
                 print(new_row)
                 transactions_list.append(
                     [new_row[DATE_COL], new_row[MEMO_COL], new_row[PAYEE_COL], new_row[AMOUNT_COL]]
-                    )
-            write_file_ynab(transactions_list, new_csv)
+                )
+
+        write_file_ynab(transactions_list, new_csv)
+
+def convert_row(row):
+    new_row = row.split(';')[:-2]
+    format_expense_amount(new_row)
+    # remove Data księgowania
+    new_row.pop(1)
+    # merge Numer konta with Memo
+    merge_accountid_with_memo(new_row)
+    # REGULARNE OSZCZĘDZANIE
+    replace_regularne(new_row, KONTO_REGULARNE)
+    # Credit Card pay up
+    replace_credit_payup(new_row, KARTA)
+    # Kapitalizacja & Odsetki
+    replace_other(new_row, KAPITALIZACJA)
+    replace_other(new_row, PODATEK_ODSETKI)
+    # if Payee empty, move Memo there
+    populate_payee_if_empty(new_row)
+    # transfer to accounts
+    replace_account(new_row, KONTO_1)
+    replace_account(new_row, KONTO_2)
+    replace_account(new_row, KONTO_3)
+
+    return new_row
+
+def format_expense_amount(row):
+    row[OLD_AMOUNT_COL] = row[OLD_AMOUNT_COL].replace(',', '.').replace(' ', '')
+
+def populate_payee_if_empty(row):
+    if row[PAYEE_COL] == '"  "':
+        row[PAYEE_COL] = row[MEMO_COL]
+        row[MEMO_COL] = ''
+
+def merge_accountid_with_memo(row):
+    if row[NUMER_KONTA_COL] != '':
+        row[MEMO_COL] = row[MEMO_COL] + row[NUMER_KONTA_COL]
 
 def replace_account(row, account):
     if INTERNAL_TRANSFER in row[OPIS_OPERACJI_COL] and (account["id"] in row[NUMER_KONTA_COL]):
         row[PAYEE_COL] = 'Transfer: {}'.format(account["name"])
+
+def replace_regularne(row, account):
+    if KONTO_REGULARNE["id"] in row[OPIS_OPERACJI_COL]:
+        row[PAYEE_COL] = 'Transfer: {}'.format(account["name"])
+
 def replace_other(row, word):
     if word in row[OPIS_OPERACJI_COL]:
         row[PAYEE_COL] = word.capitalize()
+
+def replace_credit_payup(row, card):
+    if SPLATA_KART in row[OPIS_OPERACJI_COL] and card["id"] in row[MEMO_COL]:
+        row[PAYEE_COL] = 'Transfer: {}'.format(card["name"])
 
 def write_file_ynab(data, new_csv):
     ynab_filename = YNAB_FILENAME_PREFIX + new_csv
