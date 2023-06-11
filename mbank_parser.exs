44198021c5e136e1d18cf14603f66dcf78fb5a88
Anna Nowak <matisnape@wp.pl>
2023-06-11 21:19:26 +0200

    Support all operations

--- mbank_parser.exs
+++ mbank_parser.exs
@@ -9,7 +9,7 @@ Mix.install(
 )
 
 defmodule MbankParser do
-  @doc """
+  @moduledoc """
   Parses an mbank CSV file and saves a processed version of the file in UTF-8 format.
 
   ## Examples
@@ -17,16 +17,22 @@ defmodule MbankParser do
       iex> MbankParser.parse("example.csv")
 
   """
+
+  @doc """
+  supported_operations = [
+    "PRZELEW REGULARNE OSZCZ",
+    "PRZELEW NA TWOJE CELE",
+    "RĘCZNA SPŁATA KARTY KREDYT.",
+    "KAPITALIZACJA ODSETEK",
+    "PODATEK OD ODSETEK KAPITAŁOWYCH",
+    "PRZELEW WŁASNY",
+    "PRZELEW WEWNĘTRZNY PRZYCHODZĄCY"
+  ]
+  """
+
   @ynab_headers ~w(DATE MEMO PAYEE AMOUNT)
   @ynab_delimiter ","
   @ynab_filename_prefix "YNAB_ready_"
-  @owner "ANNA MARIA NOWAK"
-  @date_col 0
-  @opis_operacji_col 1
-  @memo_col 2
-  @payee_col 3
-  @numer_konta_col 4
-  @amount_col 5
 
   # def process(file_path) do
   #   file_path
@@ -44,10 +50,10 @@ defmodule MbankParser do
     |> Stream.each(fn item ->
       # Why there's a bitstring and not a string
       :erlyconv.to_unicode(:cp1250, item)
-      |> transform()
-
-      # |> IO.write()
+      |> IO.inspect()
+      |> prepare()
     end)
+    # |> CSV.encode(headers: @ynab_headers, separators: [",", ";"])
     |> Enum.to_list()
 
     # |> IO.inspect(label: "dropped")
@@ -56,6 +62,12 @@ defmodule MbankParser do
     # |> CSV.encode(headers: ["DATE", "MEMO", "PAYEE", "AMOUNT"], separators: [",", ";"])
     # |> IO.inspect(label: "encoded")
     # |> write_file(file_path)
+  end
+
+  defp write_file(data, file_path) do
+    file_name = "YNAB_ready_" <> Path.basename(file_path)
+    full_path = Path.join(Path.dirname(file_path), file_name)
+    File.write!(full_path, data, [:utf8])
   end
 
   # def parse2(file_path) do
@@ -82,35 +94,107 @@ defmodule MbankParser do
   #   # )
   # end
 
+  defp prepare(row) do
+    [_, date, opis_operacji, memo, payee, numer_konta, amount, _, _] = String.split(row, ";")
+
+    %{date: date, memo: memo, payee: payee, amount: amount} =
+      %{
+        date: date,
+        opis_operacji: opis_operacji,
+        memo: sanitize(memo),
+        payee: sanitize(payee),
+        numer_konta: sanitize(numer_konta),
+        amount: format_amount(amount)
+      }
+      |> initial_transformation()
+      |> transform_operation()
+
+    [date, memo, payee, amount]
+  end
+
+  defp transform_operation(
+         %{opis_operacji: "PRZELEW WŁASNY", numer_konta: account_id} = transaction
+       ) do
+    transform_internal(transaction, account_id)
+  end
+
+  defp transform_operation(
+         %{opis_operacji: "PRZELEW WEWNĘTRZNY PRZYCHODZĄCY", numer_konta: account_id} =
+           transaction
+       ) do
+    transform_internal(transaction, account_id)
+  end
+
+  defp transform_operation(
+         %{opis_operacji: "PRZELEW REGULARNE OSZCZ", numer_konta: account_id} = transaction
+       ) do
+    transform_internal(transaction, account_id)
+  end
+
+  defp transform_operation(
+         %{opis_operacji: "RĘCZNA SPŁATA KARTY KREDYT.", payee: card_id} = transaction
+       ) do
+    transform_internal(transaction, card_id)
+  end
+
+  defp transform_operation(%{opis_operacji: "ZAKUP PRZY UŻYCIU KARTY"} = transaction) do
+    transaction
+  end
+
+  defp transform_operation(%{opis_operacji: "KAPITALIZACJA ODSETEK"} = transaction) do
+    Map.merge(transaction, %{payee: transaction.opis_operacji})
+  end
+
+  defp transform_operation(%{opis_operacji: "PODATEK OD ODSETEK KAPITAŁOWYCH"} = transaction) do
+    Map.merge(transaction, %{payee: transaction.opis_operacji})
+  end
+
+  defp transform_operation(transaction), do: transaction
+
+  defp transform_internal(transaction, account_id) do
+    accounts()
+    |> Enum.find(fn {key, value} -> String.contains?(account_id, value.id) end)
+    |> case do
+      {_account, %{id: _, name: account_name}} ->
+        Map.merge(transaction, %{payee: format_transfer(account_name)})
+
+      _other ->
+        IO.puts("Unknown account: #{account_id}")
+        transaction
+    end
+  end
+
+  defp initial_transformation(transaction) do
+    transaction
+    |> populate_payee_if_empty()
+    |> merge_accountid_with_memo()
+  end
+
+  defp populate_payee_if_empty(%{payee: "", memo: memo} = row) do
+    Map.merge(row, %{payee: memo, memo: ""})
+  end
+
+  defp populate_payee_if_empty(row), do: row
+
+  defp merge_accountid_with_memo(%{numer_konta: ""} = row), do: row
+
+  defp merge_accountid_with_memo(%{numer_konta: numer_konta, memo: memo} = row) do
+    Map.merge(row, %{memo: "#{memo} #{numer_konta}"})
+  end
+
+  defp accounts do
+    %{
+      ekonto: %{id: "", name: "Ekonto"}
+    }
+  end
+
+  # Helpers
+
   defp drop_metadata(stream) do
     stream
     |> Stream.drop(38)
     |> Stream.drop(-5)
   end
-
-  def transform(row) do
-    [_, date, opis_operacji, memo, payee, numer_konta, amount, _, _] = String.split(row, ";")
-
-    %{
-      date: date,
-      opis_operacji: opis_operacji,
-      memo: memo,
-      payee: payee,
-      numer_konta: numer_konta,
-      amount: format_amount(amount)
-    }
-    |> populate_payee_if_empty
-    |> merge_accountid_with_memo
-
-    # |> [date, opis_operacji, memo, payee, numer_konta, format_amount(amount)]
-    |> IO.inspect(label: "newrow")
-  end
-
-  defp populate_payee_if_empty(%{payee: "  ", memo: memo} = row) do
-    Map.merge(row, %{payee: memo, memo: ""})
-  end
-
-  defp populate_payee_if_empty(row), do: row
 
   defp format_amount(amount) do
     amount
@@ -118,31 +202,19 @@ defmodule MbankParser do
     |> String.replace(" ", "")
   end
 
-  defp merge_accountid_with_memo(%{numer_konta: ""} = row), do: row
+  defp sanitize(string) do
+    boring_strings = [
+      "'",
+      "\""
+    ]
 
-  defp merge_accountid_with_memo(%{numer_konta: numer_konta, memo: memo} = row) do
-    Map.merge(row, %{memo: memo <> numer_konta})
+    string
+    |> String.replace(boring_strings, "")
+    |> String.split()
+    |> Enum.join(" ")
   end
 
-  defp rename_internal_transfer(%{opis_operacji: opis_operacji, numer_konta: numer_konta} = row) do
-  end
-
-  def write_file(data, file_path) do
-    file_name = "YNAB_ready_" <> Path.basename(file_path)
-    full_path = Path.join(Path.dirname(file_path), file_name)
-    File.write!(full_path, data, [:utf8])
-  end
-
-  defp operations do
-    %{
-      regularne: "PRZELEW REGULARNE OSZCZ",
-      splata_kart: "RĘCZNA SPŁATA KARTY KREDYT.",
-      kapitalizacja: "KAPITALIZACJA ODSETEK",
-      podatek_odsetki: "PODATEK OD ODSETEK",
-      internal_transfer: "PRZELEW WŁASNY",
-      internal_incoming: "PRZELEW WEWNĘTRZNY PRZYCHODZĄCY"
-    }
-  end
+  defp format_transfer(account_name), do: "Transfer: " <> account_name
 end
 
 # Call the function with the provided file path
